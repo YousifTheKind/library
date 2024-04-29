51ccc706d366cf8bb394fab3842c63753c99f6b8
YousifTheKind <yousifmoeltayeb@gmail.com>
2024-04-29 15:34:52 +0300

    Add button
    add button opens a form
    user inputs data in the form
    data is used to create a new object
    object is pushed into the array of books
    
    when displayBook is called books are
    added to the table

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ function Book(title, author, pages, read) {
     this.author = author;
     this.pages = pages;
     this.read = function() {
-      if(read) {return "read"}
-      else {return "not read yet"}
+      if(read) {return "yes"}
+      else {return "no, read yet"}
     };
     this.info = function () {
       let info = `${this.title} by ${this.author}, ${this.pages} pages, ${this.read()}`
@@ -14,16 +14,45 @@ function Book(title, author, pages, read) {
     }
   };
   
+  function addBookToLibrary(title, author, pages, read) {
 
-  function addBookToLibrary() {
-    let title = prompt("Type Title: ");
-    let author = prompt("Type author: ");
-    let pages = prompt("Type pages: ");
-    let read = prompt("did you read it?(true or false): ");
-    read = (read === "true")
+    const bookObject = new Book(title, author, pages, read);
 
-    const Outliers = new Book(title, author, pages, read);
-
-    myLibrary.push(Outliers)
-    console.log(myLibrary)
+    myLibrary.push(bookObject)
 }
+
+
+function displayBooks() {
+    const table = document.querySelector(".myTable").querySelector(".tableBody");
+    for (let index in myLibrary) {
+        let tr = table.insertRow();
+        tr.insertCell().textContent = myLibrary[index].title;
+        tr.insertCell().textContent = myLibrary[index].author;
+        tr.insertCell().textContent = myLibrary[index].pages;
+        tr.insertCell().textContent = myLibrary[index].read();
+    }
+}
+
+const dialog = document.querySelector("dialog");
+const showDialog = document.querySelector(".addButton");
+const closeDialog = document.querySelector(".cancelBtn");
+const confirmBtn = document.querySelector(".confirmBtn")
+
+showDialog.addEventListener("click", function(){
+    dialog.showModal();
+});
+
+confirmBtn.addEventListener("click", function(e) {
+    e.preventDefault();
+    let title = document.getElementById("title").value;
+    let author = document.getElementById("author").value;
+    let pages = document.getElementById("pages").value;
+    let read = document.querySelector("input[name='read']:checked").value;
+    addBookToLibrary(title, author, pages, read);
+
+    dialog.close();
+});
+
+closeDialog.addEventListener("click", function(){
+    dialog.close();
+});
