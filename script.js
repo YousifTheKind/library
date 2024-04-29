69379eba5a43ee006ba277f7d326fe7d72250631
YousifTheKind <yousifmoeltayeb@gmail.com>
2024-04-29 17:10:24 +0300

    add remove and change status buttons to each table row
    fixed read status bug
    implemented remove button

--- script.js
+++ script.js
@@ -1,12 +1,30 @@
 const myLibrary = [];
 
+
+const table = document.querySelector(".myTable").querySelector(".tableBody");
+
+const removeBtn = document.createElement('button');
+removeBtn.type = 'button'
+removeBtn.textContent = "Remove";
+
+const toggleReadStatus = document.createElement('button')
+toggleReadStatus.textContent = "Read Status"
+
+const dialog = document.querySelector("dialog");
+const showDialog = document.querySelector(".addButton");
+const closeDialog = document.querySelector(".cancelBtn");
+const confirmBtn = document.querySelector(".confirmBtn")
+
+
+
+
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = function() {
-      if(read) {return "yes"}
-      else {return "no, read yet"}
+      if(read === "true") {return "Yes"}
+      else {return "No"}
     };
     this.info = function () {
       let info = `${this.title} by ${this.author}, ${this.pages} pages, ${this.read()}`
@@ -23,20 +41,19 @@ function Book(title, author, pages, read) {
 
 
 function displayBooks() {
-    const table = document.querySelector(".myTable").querySelector(".tableBody");
     for (let index in myLibrary) {
         let tr = table.insertRow();
         tr.insertCell().textContent = myLibrary[index].title;
         tr.insertCell().textContent = myLibrary[index].author;
         tr.insertCell().textContent = myLibrary[index].pages;
         tr.insertCell().textContent = myLibrary[index].read();
+        tr.insertCell(-1).append(removeBtn, toggleReadStatus);
     }
 }
 
-const dialog = document.querySelector("dialog");
-const showDialog = document.querySelector(".addButton");
-const closeDialog = document.querySelector(".cancelBtn");
-const confirmBtn = document.querySelector(".confirmBtn")
+removeBtn.addEventListener("click", function(e){
+        e.target.closest("tr").toggle()
+});
 
 showDialog.addEventListener("click", function(){
     dialog.showModal();
@@ -48,6 +65,8 @@ confirmBtn.addEventListener("click", function(e) {
     let author = document.getElementById("author").value;
     let pages = document.getElementById("pages").value;
     let read = document.querySelector("input[name='read']:checked").value;
+    console.log(read)
+
     addBookToLibrary(title, author, pages, read);
 
     dialog.close();
